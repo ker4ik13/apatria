7e9c5536f54bea8d4e7887b7a953d9e0b0954d34
ker4ik <kekustop228@mail.ru>
2023-09-15 19:00:29 +0300

    feat: добавлен скролл, исправлен отпечаток и глаз

--- src/widgets/SliderPage/index.tsx
+++ src/widgets/SliderPage/index.tsx
@@ -1,20 +1,20 @@
 import styles from "./SliderPage.module.scss";
 import sliderBg from "@/images/slider/sliderBg.png";
-// import slider1Img from "@/images/slider/slider1.png";
-// import slider2Img from "@/images/slider/slider2.png";
-// import slider3Img from "@/images/slider/slider3.png";
-// import slider4Img from "@/images/slider/slider4.png";
-// import slider5Img from "@/images/slider/slider5.png";
-// import slider6Img from "@/images/slider/slider6.png";
+import slider1Img from "@/images/slider/slider1.png";
+import slider2Img from "@/images/slider/slider2.png";
+import slider3Img from "@/images/slider/slider3.png";
+import slider4Img from "@/images/slider/slider4.png";
+import slider5Img from "@/images/slider/slider5.png";
+import slider6Img from "@/images/slider/slider6.png";
 
-// const allSliderImg: string[] = [
-//   slider1Img,
-//   slider2Img,
-//   slider3Img,
-//   slider4Img,
-//   slider5Img,
-//   slider6Img,
-// ];
+const allSliderImg: string[] = [
+  slider1Img,
+  slider2Img,
+  slider3Img,
+  slider4Img,
+  slider5Img,
+  slider6Img,
+];
 
 const Slider = () => {
   return (
@@ -23,6 +23,60 @@ const Slider = () => {
       <div className={styles.content}>
         <h2 className={styles.title}>ZPRs</h2>
         <p className={styles.description}>зоны распределения</p>
+        <div className={styles.slider}>
+          <div className={styles.items}>
+            {allSliderImg.map((item, index) => (
+              <div
+                className={styles.sliderItem}
+                key={index}
+                id={`slide-${index + 1}`}
+                onClick={(event) => {
+                  event.currentTarget.parentElement?.classList.toggle(
+                    styles.overflow,
+                  );
+                  event.currentTarget.classList.toggle(styles.active);
+                }}
+              >
+                <img src={item} className={styles.img} />
+                <p className={styles.text}>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Rerum veritatis, non, eveniet laudantium nesciunt dolore
+                  dignissimos sed provident architecto aspernatur magnam
+                  accusantium itaque qui reiciendis corrupti dolorum eaque nam
+                  iusto!
+                </p>
+              </div>
+            ))}
+          </div>
+          {/* <div className={styles.indicators}>
+            <a href='#slide-1' className={styles.indicator}>
+              1
+            </a>
+            <a href='#slide-2' className={styles.indicator}>
+              2
+            </a>
+            <a href='#slide-3' className={styles.indicator}>
+              3
+            </a>
+            <a href='#slide-4' className={styles.indicator}>
+              4
+            </a>
+            <a href='#slide-5' className={styles.indicator}>
+              5
+            </a>
+            <a href='#slide-6' className={styles.indicator}>
+              6
+            </a>
+          </div> */}
+          {/* <div className={styles.buttons}>
+            <button type='button' className={styles.button} onClick={() => {}}>
+              Left
+            </button>
+            <button type='button' className={styles.button}>
+              Right
+            </button>
+          </div> */}
+        </div>
       </div>
     </div>
   );
