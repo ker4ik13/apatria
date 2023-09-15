4563ab37acb3b5d2a6ab1a4631ac00c619c37c22
ker4ik <kekustop228@mail.ru>
2023-09-15 15:43:13 +0300

    fix: фикс для прода

--- src/widgets/SliderPage/index.tsx
+++ src/widgets/SliderPage/index.tsx
@@ -1,20 +1,20 @@
 import styles from "./SliderPage.module.scss";
 import sliderBg from "@/images/slider/sliderBg.png";
-import slider1Img from "@/images/slider/slider1.png";
-import slider2Img from "@/images/slider/slider2.png";
-import slider3Img from "@/images/slider/slider3.png";
-import slider4Img from "@/images/slider/slider4.png";
-import slider5Img from "@/images/slider/slider5.png";
-import slider6Img from "@/images/slider/slider6.png";
+// import slider1Img from "@/images/slider/slider1.png";
+// import slider2Img from "@/images/slider/slider2.png";
+// import slider3Img from "@/images/slider/slider3.png";
+// import slider4Img from "@/images/slider/slider4.png";
+// import slider5Img from "@/images/slider/slider5.png";
+// import slider6Img from "@/images/slider/slider6.png";
 
-const allSliderImg: string[] = [
-  slider1Img,
-  slider2Img,
-  slider3Img,
-  slider4Img,
-  slider5Img,
-  slider6Img,
-];
+// const allSliderImg: string[] = [
+//   slider1Img,
+//   slider2Img,
+//   slider3Img,
+//   slider4Img,
+//   slider5Img,
+//   slider6Img,
+// ];
 
 const Slider = () => {
   return (
